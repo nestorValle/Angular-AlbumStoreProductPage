6fffc36a8afb8c784ebb4aadbfe1fc4fc3064d4e
Nestor Valle <nvalleVPN@bofifederalbank.com>
2018-11-29 20:38:39 -0800

    list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,17 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrls = '../assets/products.json';
   constructor(private _http: Http) {}
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrls).map(response => <Product[]>response.json());
   }
 }
